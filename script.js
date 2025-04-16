9b810719d9b71bf3f9360542008e6d5eedf95a10
gutomachaddo <augustomachado.com.br@gmail.com>
2025-04-16 18:31:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const socialIcons = document.querySelectorAll('.social-icon');
 
-    socialIcons.forEach(icon => {
-        icon.addEventListener('mouseenter', function() {
-            this.classList.add('hover-effect');
-        });
-
-        icon.addEventListener('mouseleave', function() {
-            this.classList.remove('hover-effect');
-        });
-    });
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> e5ed9eee9727732e5a86bb031e810e7e54e5dc3b
