946193a8e65e26649977d951e086c07ddfb6c55e
gutomachaddo <augustomachado.com.br@gmail.com>
2025-04-16 19:26:17 -0300

    portifólio_final

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded'), function() {
+    const socialIcons = document.querySelectorAll('.social-icon');
 
+    socialIcons.forEach(icon => {
+        icon.addEventListener('mouseenter', function() {
+            this.classList.add('hover-effect');
+        });
+
+        icon.addEventListener('mouseleave', function() {
+            this.classList.remove('hover-effect');
+        });
+    });
+}
